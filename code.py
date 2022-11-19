f764d9e80f42cf10df9b9505cbf798e5f69e26b3
Kévin <56440487+kevinbdx35@users.noreply.github.com>
2022-11-19 20:07:29 +0100

    Create code.py

---
+++ code.py
@@ -0,0 +1,11 @@
+def find_missing_numbers(n):
+    numbers = set(n)
+    length = len(n)
+    output = []
+    for i in range(1, n[-1]):
+        if i not in numbers:
+            output.append(i)
+    return output
+    
+list_of_numbers = [1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 13, 14, 16]
+print(find_missing_numbers(list_of_numbers))
